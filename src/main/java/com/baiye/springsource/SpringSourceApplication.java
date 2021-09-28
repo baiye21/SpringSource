692497a474c8a0d4732ac9380ee079b9f2527b04
baiye21 <baiye_21@163.com>
2021-09-28 21:05:19 +0800

    first commit

---
+++ src/main/java/com/baiye/springsource/SpringSourceApplication.java
@@ -0,0 +1,13 @@
+package com.baiye.springsource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSourceApplication.class, args);
+    }
+
+}
